d14d9c8059ea442705f00a1cd7d4d1a4c8910be0
LazyStudent23 <wizard.alex2016@gmail.com>
2024-11-12 22:24:29 +0200

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 
-import './App.css'
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
+import "./App.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "./redux/contactsOps";
 
-function App() {
-  
-}
+const App = () => {
+  const dispatch = useDispatch();
 
-export default App
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+        <SearchBox />
+      <ContactList />
+    </div>
+  );
+};
+
+export default App;
